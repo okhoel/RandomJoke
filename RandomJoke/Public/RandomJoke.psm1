f16447122b930593f86c4ca650295e4e5091d8c1
Ole Kristian Hoel <okhoel@gmail.com>
2021-10-03 23:55:14 +0200

    Added support for more languages

--- RandomJoke/Public/RandomJoke.psm1
+++ RandomJoke/Public/RandomJoke.psm1
@@ -1,4 +1,14 @@
 function Get-RandomJoke{
-    $jokes = Get-Jokes
-    invoke-command -ScriptBlock (Get-Random $jokes)
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [Language]$Language = "Norwegian"
+    )
+    $jokes = Get-Jokes -Language $Language
+    if ($jokes.length -gt 0){
+        (Get-Random $jokes).toString()
+    }
+    else {
+        "No jokes in $Language"
+    }
 }
