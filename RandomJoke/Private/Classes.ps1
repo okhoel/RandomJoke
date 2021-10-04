9ea7b9704cb416fc0ba641813de5fe74d07d4a0f
Ole Kristian Hoel <okhoel@gmail.com>
2021-10-05 00:48:56 +0200

    Put class definition in a separate file

---
+++ RandomJoke/Private/Classes.ps1
@@ -0,0 +1,16 @@
+﻿class Joke {
+    [Language]$Language
+    [ScriptBlock]$Content
+
+    Joke(
+        [Language]$l,
+        [ScriptBlock]$c    
+    ){
+        $this.Language = $l
+        $this.Content = $c
+    }
+
+    [string]toString() {
+        return (Invoke-Command -ScriptBlock $this.Content)
+    }
+}
